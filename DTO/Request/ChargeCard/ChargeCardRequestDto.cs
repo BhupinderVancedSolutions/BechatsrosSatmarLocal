8d565d02ebb9892f8dab5ae798fa55f3613de9e7
satnamsinghvancedsolutions <satnamsinghvancedsolutions>
2024-02-28 15:20:13 +0530

    Rk-modify js and rename request dto name

---
+++ DTO/Request/ChargeCard/ChargeCardRequestDto.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace DTO.Request
+{
+    public class ChargeCardRequestDto
+    {
+        public int CardKnoxId { get; set; }
+        [Required(ErrorMessage = "Please enter a Name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a Address")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Please enter a Email.")]
+        public string Email { get; set; }
+        [Display(Name = "Expiry Month")]
+        [Required(ErrorMessage = "Please enter Expiry Month.")]
+        public long ExpMonth { get; set; }
+        [Display(Name = "Expiry Year")]
+        [Required(ErrorMessage = "Please enter Expiry Year.")]
+        public long ExpYear { get; set; }
+        [Display(Name = "Credit Card Number")]
+        [Required(ErrorMessage = "Please enter Credit Card Number.")]
+        public string CreditCardNumber { get; set; }
+        [Required(ErrorMessage = "Please enter CVV.")]
+        public string Cvv { get; set; }
+        [Required(ErrorMessage = "Please enter a PhoneNumber.")]
+        public string PhoneNumber { get; set; }
+        public string CardLast4Digit { get; set; }
+        public string ExpDate { get; set; }
+        [Required(ErrorMessage = "Please enter a FirstName.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter a LastName.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Please enter a City.")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Please enter a Zip.")]
+        public int Zip { get; set; }
+
+    }
+}
---
+++ Presentation/wwwroot/assets/js/ChargeCard/charge-card.js
@@ -0,0 +1,59 @@
+﻿function addNewform() { 
+    debugger
+    $.ajax({
+        type: "GET",
+        url: "/CardKnox/CardKnoxDonationform",
+        dataType: "html",
+        success: function (data) {
+            $("#donation-method").html("");
+            $("#donation-method").html(data);
+            $(".mask-number").inputmask("(999) 999-9999");
+        },
+        error: function () {
+            //ShowLoader(false);
+        }
+    });
+}
+
+
+
+function ChargeCard() {
+    if ($(".charge-card").valid()) {
+        ProcessChargeCard();
+    
+    
+    }
+    
+   
+ 
+}
+function ProcessChargeCard() {
+   
+        var formData = $("#donation-method").serializeJSON();
+        $.ajax({
+            type: "POST",
+            url: "/CardKnox/CardKnoxDonation",
+            data: { cardKnoxDonationRequest: formData },
+            dataType: "json",
+
+            success: function (data) {
+
+                //addCreditCardAndSendEmail(formData, data);
+                if (data.status) {
+                    toastr.success("Payment successfully.");
+                }
+            },
+            error: function () {
+
+            }
+        });
+   
+}
+
+function Checkbox() {
+    debugger
+}
+
+
+
+
